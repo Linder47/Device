144a26c2ebb47d2e958abd023db28fd744d7af1a
Linder47 <catty97@mail.ru>
2017-09-26 21:33:37 +0300

    done popup 'write us'

--- js/main.js
+++ js/main.js
@@ -1,7 +1,7 @@
 function initMap() {
-    var coordinates = { lat: 34.866910, lng: -111.792495 },
-    map = new google.maps.Map(document.getElementById('map'), {
-      zoom: 7,
+    var coordinates = { lat: 55.687104, lng: 37.529618 },
+    map = new google.maps.Map(document.getElementById('google-map'), {
+      zoom: 17,
       center: coordinates
     }),
   
@@ -10,3 +10,34 @@ function initMap() {
         map: map
       });
   }
+
+  var buttonMapClose = document.querySelector(".map-popup__btn-close");
+  var mapPopup = document.querySelector(".map-popup");
+  var mapImage = document.querySelector("#map");
+  var writeUsPopup = document.querySelector(".write-us-popup");
+  var buttonWriteUsClose = document.querySelector(".write-us-popup__btn-close");
+  var buttonWriteUs = document.querySelector(".btn--write-us");
+
+  buttonMapClose.addEventListener("click", function(event) {
+    event.preventDefault();
+    mapPopup.classList.add("map-popup--close");
+  });
+
+mapImage.addEventListener("click", function(event) {
+    event.preventDefault();
+    if (mapPopup.classList.contains("map-popup--close")) {
+    mapPopup.classList.remove("map-popup--close");
+    } else {
+      mapPopup.classList.add("map-popup--close");
+    }
+  });
+
+  buttonWriteUsClose.addEventListener("click", function(event) {
+    event.preventDefault();
+    writeUsPopup.classList.add("write-us-popup--close");
+  });
+
+  buttonWriteUs.addEventListener("click", function(event) {
+    event.preventDefault();
+    writeUsPopup.classList.remove("write-us-popup--close");
+  });
